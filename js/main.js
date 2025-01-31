8b5003b903ed505300158ab48aa10dbeb8fbe6d3
Redphyco <lakejarrett.webster@gmail.com>
2025-01-31 16:10:22 -0700

    added a modal for my picture

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@
 function helloWorld(){
     //window.alert("Hello World");
 }
+
+const modal= document.getElementById('modal')
+const me = document.getElementById('me')
+const closebtn = document.querySelector('.close')
+
+me.addEventListener('click', () => {
+    modal.style.display = 'block'
+});
+
+closebtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', () => {
+    if (event.target == modal){
+        modal.style.display = 'none';
+    }
+});
